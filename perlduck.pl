a15e244471c5d8d7972925a61a96393e3c66abff
bearachute <sjourand@gmail.com>
2017-06-23 22:41:31 -0500

    hey i made it work :duck: :tada: :thumbsup:

--- perlduck.pl
+++ perlduck.pl
@@ -4,6 +4,18 @@ my @count = qw( eenie miney moe );
 my @duck = qw( huey dewey louie);
 my @game = qw( duck duck grayduck );
 
+foreach my $count (@count) {
+print $count, "\n";
+}
+
+foreach my $duck (@duck) {
+print $duck, "\n";
+}
+
+foreach my $game (@game) {
+print $game, "\n";
+}
+
 #say @count;
 #say @duck;
 #say @game;
